e91db7f5894e65cc9eda5a56d446df1fa61b47c5
Eugene <oleynik.zh10@gmail.com>
2021-07-27 12:47:37 +0300

    UersRepository updated

---
+++ Repositories/IUsersRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MoneyKeeper.Dtos;
+using MoneyKeeper.Entities;
+
+namespace MoneyKeeper.Repositories
+{
+	public interface IUsersRepository
+	{
+		Task<User> GetUserAsync(int id);
+
+		Task<User> GetUserAsync(string email);
+
+		Task<IEnumerable<User>> GetUsersAsync();
+
+		Task CreateUserAsync(User userDto);
+
+		Task UpdateUserAsync(int id, User userDto);
+
+		Task DeleteUserAsync(int id);
+
+	}
+}
