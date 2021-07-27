07d2e47d8af99e5e81c5ab521a89ee91d7e3cd71
Eugene <oleynik.zh10@gmail.com>
2021-07-27 14:14:08 +0300

    sha256 method added

---
+++ Extensions/StringExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyKeeper.Extensions
+{
+	public static class StringExtension
+	{
+        public static string AsSHA256Hash(this string str)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(str);
+            data = new System.Security.Cryptography.SHA256Managed().ComputeHash(data);
+
+            string hash = Encoding.ASCII.GetString(data);
+            return hash;
+        }
+    }
+}
