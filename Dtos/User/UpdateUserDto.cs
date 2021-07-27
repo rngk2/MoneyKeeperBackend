f9c53eeaa07c087d39fed3b864404049d1fb9b01
Eugene <oleynik.zh10@gmail.com>
2021-07-27 12:45:17 +0300

    User entity & dtos added

---
+++ Dtos/User/UpdateUserDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyKeeper.Dtos
+{
+	public record UpdateUserDto
+	{
+		public string FirstName { get; init; }
+
+		public string LastName { get; init; }
+
+		[EmailAddress]
+		public string Email { get; init; }
+
+		[DataType(DataType.Password)]
+		[
+			RegularExpression("^((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])|(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[^a-zA-Z0-9])|(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])|(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])).{8,16}$",
+			ErrorMessage = "Passwords must be 8-16 characters and contain at 3 of 4 of the following: upper case (A-Z), lower case (a-z), number (0-9) and special character (e.g. !@#$%^&*)")
+		]
+		public string Password { get; init; }
+	}
+}
---
+++ Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyKeeper.Entities
+{
+	public record User
+	{
+		public int Id { get; init; }
+
+		public string FirstName { get; init; }
+		
+		public string LastName { get; init; }
+		
+		public string Email { get; init; }
+		
+		public string Password { get; init; }
+	
+	}
+}
