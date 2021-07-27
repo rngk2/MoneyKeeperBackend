19772fd6fdc11564b9c3ade8c145bdb9498af206
Eugene <oleynik.zh10@gmail.com>
2021-07-27 14:13:19 +0300

    asDto ext added for User

---
+++ Extensions/UserExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MoneyKeeper.Dtos;
+using MoneyKeeper.Entities;
+
+namespace MoneyKeeper.Extensions
+{
+	public static class UserExtension
+	{
+		public static UserDto AsDto(this User user) => new UserDto
+        {
+            Id        = user.Id,
+            FirstName = user.FirstName,
+            LastName  = user.LastName,
+            Email     = user.Email,
+            Password  = user.Password
+        };
+    }
+}
