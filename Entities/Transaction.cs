acd86cfb7a9715d78e582ceb93ba1e6e6f2296b7
Eugene <oleynik.zh10@gmail.com>
2021-07-27 16:05:13 +0300

    Category & Transaction entities added

---
+++ Entities/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyKeeper.Entities
+{
+	public record Transaction
+	{
+		public int Id { get; init; }
+
+		public int CategoryId { get; init; }
+
+		public decimal Amount { get; init; }
+
+		public DateTime Timestamp { get; init; }
+	}
+}
