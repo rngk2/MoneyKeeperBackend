6436b2679ed6e04e78f96a6bd4dfb7a6dcfd5d88
Eugene <oleynik.zh10@gmail.com>
2021-07-27 18:44:32 +0300

    DapperRepository added

---
+++ Repositories/DapperRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Extensions.Options;
+using MoneyKeeper.Settings;
+
+namespace MoneyKeeper.Repositories
+{
+	public class DapperRepository<T>
+	{
+		private readonly string connectionString;
+
+		public DapperRepository(IOptions<DapperSettings> options)
+		{
+			connectionString = options.Value.DefaultConnection;
+		}
+
+		private SqlConnection GetDbConnection()
+		{
+			return new SqlConnection(connectionString);
+		}
+
+		public async Task<IEnumerable<T>> QueryAny(string sql, object @params = null)
+		{
+			await using var conn = GetDbConnection();
+			return await conn.QueryAsync<T>(sql, parameters);
+		}
+
+		public async Task ExecuteAny(string sql, object @params = null)
+		{
+			await using var conn = GetDbConnection();
+			await conn.ExecuteAsync(sql, @params);
+		}
+
+	}
+}
---
+++ Repositories/Users/DapperUsersRepository.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using MoneyKeeper.Entities;
+using MoneyKeeper.Settings;
+
+namespace MoneyKeeper.Repositories
+{
+	public class DapperUsersRepository : DapperRepository<User>, IUsersRepository
+	{
+		private readonly string connectionString;
+
+		public DapperUsersRepository(IOptions<DapperSettings> options) : base(options) { }
+
+		public async Task<User> GetUser(int id)
+		{
+			const string getUserQuery = "select * from users where Id = @id";
+			return (await QueryAny(getUserQuery, new { id })).FirstOrDefault();
+		}
+
+		public async Task<User> GetUser(string email)
+		{
+			const string getUserQuery = "select * from users where Email = @email";
+			return (await QueryAny(getUserQuery, new { email })).FirstOrDefault();
+		}
+
+		public async Task<IEnumerable<User>> GetUsers()
+		{
+			const string getUsersQuery = "select * from users";
+			return await QueryAny(getUsersQuery);
+		}
+
+		public async Task CreateUser(User user)
+		{
+			const string createUserQuery = @"
+								insert into [dbo].[Users]
+									([Id], [FirstName], [LastName], [Email], [Password])
+								values 
+									(@Id, @FirstName, @LastName, @Email, @Password)
+			";
+
+			await ExecuteAny(createUserQuery, user);
+		}
+
+		public async Task UpdateUser(User userData)
+		{
+			const string updateUserQuery = @"
+					update [dbo].[Users]
+						set 
+							FirstName = @FirstName,
+							LastName = @LastName,
+							Email = @Email,
+							Password = @Password
+						where
+							Id = @Id
+							
+			";
+
+			await ExecuteAny(updateUserQuery, userData);
+		}
+
+		public async Task DeleteUser(int id)
+		{
+			const string deleteUserQuery = @"
+					delete from [dbo].[Users]
+						where
+							Id = @id
+							
+			";
+			
+			await ExecuteAny(deleteUserQuery, new { id });
+		}
+
+	}
+}
